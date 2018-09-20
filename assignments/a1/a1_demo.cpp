7aeb1f50288c5472894a5d82cb6e8db09829ab53
Angad Singh <a289sing@edu.uwaterloo.ca>
2018-09-19 20:31:55 -0400

    A1, add splash screen and trigger gameplay

--- assignments/a1/a1_demo.cpp
+++ assignments/a1/a1_demo.cpp
@@ -37,7 +37,7 @@ int main( int argc, char *argv[] ) {
 	long foreground = WhitePixel(display, screennum);
 	long background = BlackPixel(display, screennum);
 	window = XCreateSimpleWindow(display, DefaultRootWindow(display),
-                            10, 10, 500, 500, 2, foreground, background);
+                            10, 10, 1200, 800, 2, foreground, background);
 
 	// set events to monitor and display window
 	XSelectInput(display, window, ButtonPressMask | KeyPressMask);
@@ -64,11 +64,29 @@ int main( int argc, char *argv[] ) {
 	XWindowAttributes w;
 	XGetWindowAttributes(display, window, &w);
 
+	GC gc1 = XCreateGC(display, window, 0, 0);
+	XSetForeground(display, gc1, WhitePixel(display, screennum));
+	XSetBackground(display, gc1, BlackPixel(display, screennum));
+	XSetFillStyle(display, gc1, FillSolid);
+	XSetLineAttributes(display, gc1,
+	                   1, LineSolid, CapButt, JoinRound);
+
+XClearWindow(display, window);
+	XFillRectangle(display, window, gc1, 50, 50, 1255, 615);
+	XFillArc(display, window, gc1, 
+				ballPos.x - ballSize/2, 
+				ballPos.y - ballSize/2, 
+				ballSize, ballSize,
+				0, 360*64);
+    XFlush( display );
+
 	// save time of last window paint
 	unsigned long lastRepaint = 0;
 
 	// event handle for current event
 	XEvent event;
+
+	int counter = 0;
 
 	// event loop
 	while ( true ) {
@@ -88,6 +106,12 @@ int main( int argc, char *argv[] ) {
 					KeySym key;
 					char text[10];
 					int i = XLookupString( (XKeyEvent*)&event, text, 10, &key, 0 );
+
+					// start game
+					if ( i == 1 && text[0] == 'p' && counter == 0 ) {
+						counter++;
+						XClearWindow(display, window);
+					}
 
 					// move right
 					if ( i == 1 && text[0] == 'd' ) {
@@ -116,10 +140,16 @@ int main( int argc, char *argv[] ) {
 			XClearWindow(display, window);
 
 			// draw rectangle
+			if (counter == 0) {
+				XFillRectangle(display, window, gc1, 50, 50, 1255, 615);
+				XFlush( display );
+				lastRepaint = now();
+			} else {
+
 			XDrawRectangle(display, window, gc, rectPos.x, rectPos.y, 50, 50);
 
 			// draw ball from centre
-			XFillArc(display, window, gc, 
+			 XFillArc(display, window, gc1, 
 				ballPos.x - ballSize/2, 
 				ballPos.y - ballSize/2, 
 				ballSize, ballSize,
@@ -141,11 +171,12 @@ int main( int argc, char *argv[] ) {
 
 			lastRepaint = now(); // remember when the paint happened
 		}
+	}
 
 		// IMPORTANT: sleep for a bit to let other processes work
 		if (XPending(display) == 0) {
 			usleep(1000000 / FPS - (now() - lastRepaint));
 		}
 	}
-	XCloseDisplay(display);
+    XCloseDisplay(display);
 }
