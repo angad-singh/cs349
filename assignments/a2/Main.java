bea6a80227039a68c80630ce0b13b10e98f29027
Angad Singh <a289sing@edu.uwaterloo.ca>
2018-10-14 22:31:30 -0400

    A2, add canvas and mouse listener to canvas

--- assignments/a2/Main.java
+++ assignments/a2/Main.java
@@ -1,12 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         // App window
         JFrame frame = new JFrame("JSketch - Paint Freely!");
+        frame.getContentPane().setLayout(new BorderLayout());
         // frame.setMinimumSize(128, 128);
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,35 +37,63 @@ public class Main {
         view.add(window);
 
         // create a panel to hold all the views in the app
-        JPanel appWindow = new JPanel(new BorderLayout());
-        appWindow.setSize(800, 600);
+        // JPanel appWindow = new JPanel(new BorderLayout());
+        // appWindow.setSize(800, 600);
 
-        // Panel to hold all the toolbars
-        JPanel toolbar = new JPanel(new GridLayout(3,1));
-        toolbar.setSize(200, 600);
+        // Panel to hold all the ps
+        // JPanel toolbar = new JPanel(new GridLayout(3,1));
+        // toolbar.setSize(200, 600);
         
         Model model = new Model();
+        JToolBar toolbar = new JToolBar("toolbar");
+        toolbar.setOrientation(SwingConstants.VERTICAL);
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(3, 1));
         // Menu menu = new Menu (model);
         ToolPalette tools = new ToolPalette(model);
         LineThicknessPalette lineThickness = new LineThicknessPalette(model);
         ColorPalette colorPalette = new ColorPalette(model);
-        MyCanvas canvas = new MyCanvas(model);
+        MyCanvas canvas = new MyCanvas();
+
+        // Graphics g = frame.getGraphics();
+
+        // canvas.paint(g);
+        
 
         // ToolPalette tools1 = new ToolPalette(model);
         // ToolPalette tools2 = new ToolPalette(model);
 
-        toolbar.add(tools);
-        toolbar.add(colorPalette);
-        toolbar.add(lineThickness);
-
+        
         // appWindow.add(menu, BorderLayout.NORTH);
-        appWindow.add(toolbar, BorderLayout.WEST);
-        appWindow.add(canvas, BorderLayout.EAST);
+        // appWindow.add(toolbar, BorderLayout.WEST);
+       
+
+        // Canvas canvas = new Canvas();
+        // canvas.setBackground(new Color(255,0,0));
+
+        
+
+        // canvas.setSize(400, 400);
+        // frame.add(canvas);
+        p.add(tools);
+        p.add(colorPalette);
+        p.add(lineThickness);
+
+        toolbar.add(p);
+
+        // frame.pack();
+        // frame.setVisible(true);
+        // appWindow.add(canvas, BorderLayout.EAST);
         
         // add the panel to the app window
-        frame.getContentPane().add(appWindow);
         frame.setJMenuBar(menuBar);
-        // frame.setContentPane(canvas);
+        // JPanel panel = new JPanel();
+        // Canvas c = new Canvas();
+        // panel.add(new Canvas());
+        // frame.setContentPane(c);
+        frame.getContentPane().add(toolbar, BorderLayout.WEST);
+        frame.getContentPane().add(canvas, BorderLayout.CENTER);
+        // frame.add(appWindow);
         
         // View view = new View(model);
         // Menu menu = new Menu(model);
--- assignments/a2/MyCanvas.java
+++ assignments/a2/MyCanvas.java
@@ -2,24 +2,51 @@
 import java.io.*;
 import java.util.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.*;
-
+import java.awt.*;
 import javax.swing.*;
 
-public class MyCanvas extends JComponent implements Observer {
+public class MyCanvas extends JComponent {
 
-    private Model model;
+    // private Model model;
 
     /**
      * Create a new View.
      */
-    public MyCanvas(Model model) {
+    MyCanvas() {
+        super();
+        this.setSize(600, 600);
+        this.addMouseMotionListener(new MouseAdapter() {
+            public void mouseMoved(MouseEvent e) {
+                System.out.println("Mouse X = " + e.getX());
+                System.out.println("Mouse Y = " + e.getY());
+                repaint();
+            }
+        });
+        
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Mouse X = " + e.getX());
+                System.out.println("Mouse Y = " + e.getY());
+            }
+        });
+
+            }
+        
+    public void paintComponent(Graphics g) {
+        // super.paint(g);
+        // cast to get 2D drawing methods
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(10));
+
+        g2.drawLine(10, 10, 100, 100);
+
+        g2.setBackground(Color.BLACK);
+    }
+        // super();
         // Set up the window.
         // this.setTitle("Your program title here!");
         // this.setMinimumSize(new Dimension(128, 128));
-        this.setSize(600, 600);
         // this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Add the components
@@ -44,39 +71,14 @@ public class MyCanvas extends JComponent implements Observer {
 
         // Hook up this observer so that it will be notified when the model
         // changes.
-        this.model = model;
-        model.addObserver(this);
+        // this.model = model;
+        // model.addObserver(this);
 
-        setVisible(true);
+        // setVisible(true);
         // events
-         this.addMouseListener(new MouseAdapter(){
-            public void mouseMoved(MouseEvent e) { 
-            // new MouseMotionListener(){
         
-            // @Override
-            // public void mouseMoved(MouseEvent e) {
-            //     System.out.println("Mouse X = " + e.getX());
-            //     System.out.println("Mouse Y = " + e.getY());
-            //     repaint();
-            // }
         
-            // @Override
-            // public void mouseDragged(MouseEvent e) {
-            //     System.out.println("MouseDragged X = " + e.getX());
-            //     System.out.println("MouseDragged Y = " + e.getY());
-            //     repaint();
-            // }
-        // };() {
-        //     public void mouseMoved(MouseEvent e) {
-        //         // M.x = e.getX();
-        //         // M.y = e.getY();
-        //         // repaint();
-                System.out.println("Mouse X = " + e.getX());
-                System.out.println("Mouse Y = " + e.getY());
-            }
-        });
-        
-    }
+    
 
        
    
